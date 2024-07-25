50a433a2b0d1b1a01fc4736cbb0f3c299bc079e3
fist_it <fist_it@protonmail.com>
2024-07-25 13:25:45 +0200

    dropping tmux plugin

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
 		require('lualine').setup {
 			options = {
 				icons_enabled = true,
-				theme = 'oxocarbon',
+				theme = 'rose-pine',
 				component_separators = { left = '', right = '' },
 				section_separators = { left = '', right = '' },
 				disabled_filetypes = {
