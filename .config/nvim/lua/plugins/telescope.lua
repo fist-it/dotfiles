e39b1627f963859f47c1c1ad590c963528e2b607
fist_it <fist_it@protonmail.com>
2024-08-18 21:36:25 +0200

    telescope built in live grep instead of custom function

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,17 +7,7 @@ return {
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "find files by fzf" })
 		vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = "find files by fzf excl. gitignore" })
-		vim.keymap.set('n', '<leader>ps', function()
-				vim.ui.input({ prompt = 'Search for: ' }, function(input)
-					if input then
-						require('telescope.builtin').grep_string({ search = input })
-						vim.api.nvim_feedkeys(
-							vim.api.nvim_replace_termcodes("<Esc>", true, false, true), 'n',
-							true)
-					end
-				end)
-			end,
-			{ desc = "find phrase in files by Telescope " })
+		vim.keymap.set('n', '<leader>ps', builtin.live_grep,	{ desc = "find phrase in files by Telescope " })
 
 		vim.keymap.set('n', '<leader>k', ":Telescope keymaps<cr>", { desc = "[Telescope] - Search Keymaps" })
 	end
