f3dda7f15bcc79c6f4465907a5fb57d9fd9ee26a
fist_it <fist_it@protonmail.com>
2024-09-08 19:16:02 +0200

    temporary disable space names in sketchybar

--- .config/sketchybar/plugins/space_windows.sh
+++ .config/sketchybar/plugins/space_windows.sh
@@ -20,14 +20,14 @@ reload_workspace_icon() {
   apps=$(aerospace list-windows --workspace "$@" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
 
   icon_strip=""
-  if [ "${apps}" != "" ]; then
-    while read -r app
-    do
-      icon_strip+=" $($CONFIG_DIR/plugins/icon_map.sh "$app")"
-    done <<< "${apps}"
-  else
-    icon_strip=" —"
-  fi
+  # if [ "${apps}" != "" ]; then
+  #   while read -r app
+  #   do
+  #     icon_strip+=" $($CONFIG_DIR/plugins/icon_map.sh "$app")"
+  #   done <<< "${apps}"
+  # else
+  #   icon_strip=" —"
+  # fi
 
   icon_strip=""
   sketchybar --animate sin 10 --set space.$@ label="$icon_strip"
