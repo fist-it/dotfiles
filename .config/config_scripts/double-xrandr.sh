63be139dd43eb3d1f52d1695e8684037ac316f5c
fist_it <fist_it@protonmail.com>
2024-09-17 17:08:49 +0200

    xrandr script adapted to work with a single monitor

--- .config/config_scripts/double-xrandr.sh
+++ .config/config_scripts/double-xrandr.sh
@@ -1,3 +1,9 @@
 #!/bin/env bash
 
-xrandr --output DP-2 --primary --mode 2560x1440 --rate 144.00 --output DP-4 --left-of DP-2 --mode 2560x1440 --rate 144.00
+xrandr --output DP-2 --primary --mode 2560x1440 --rate 144.00 
+
+if [ ! -z $(xrandr --query | grep "DP-4 connected") ]; then
+  xrandr --output DP-4 --left-of DP-2 --mode 2560x1440 --rate 144.00
+else
+  xrandr --output DP-4 --off
+fi
