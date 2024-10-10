eed08818f60acc24591c4ed4edef64ae50ce550f
fist_it <fist_it@protonmail.com>
2024-10-10 21:04:02 +0200

    moved non-cask packages to nixpkgs

--- .config/nix-darwin/modules/apps.nix
+++ .config/nix-darwin/modules/apps.nix
@@ -8,17 +8,6 @@
   #
   ##########################################################################
 
-  # Install packages from nix's official package repository.
-  #
-  # The packages installed here are available to all users, and are reproducible across machines, and are rollbackable.
-  # But on macOS, it's less stable than homebrew.
-  #
-  # Related Discussion: https://discourse.nixos.org/t/darwin-again/29331
-  environment.systemPackages = with pkgs; [
-    git
-    just
-  ];
-
   # The apps installed by homebrew are not managed by nix, and not reproducible!
   # But on macOS, homebrew has a much larger selection of apps than nixpkgs, especially for GUI apps!
   homebrew = {
@@ -27,46 +16,29 @@
     onActivation = {
       autoUpdate = true;
       # 'zap': uninstalls all formulae(and related files) not listed here.
-      # cleanup = "zap";
+      cleanup = "zap";
     };
 
     taps = [
       "homebrew/services"
+      "felixkratz/formulae"
+      "nikitabobko/tap"
     ];
 
     # `brew install`
     brews = [
-      "btop"
-      "cmake"
       "docker"
       "docker-completion"
-      "fastfetch"
-      "fzf"
-      "gcc"
-      "go"
-      "llvm"
-      "lua"
-      "luajit"
-      "neovim"
-      "ninja"
-      "nmap"
-      "parallel"
-      "pure"
-      "python@3.12"
-      "ripgrep"
-      "stow"
-      "tmux"
-      "tree"
-      "vim"
-      "tree-sitter"
-      "wget"
+      "git-spice"
       "sketchybar"
     ];
 
     # `brew install --cask`
     casks = [
       "firefox@developer-edition"
+      "logitech-g-hub"
       "kitty"
+      "font-sf-pro"
       "keepassxc"
       "orbstack"
       "aerospace"
@@ -77,4 +49,41 @@
       "vlc"
     ];
   };
+
+  # Install packages from nix's official package repository.
+  #
+  # The packages installed here are available to all users, and are reproducible across machines, and are rollbackable.
+  # But on macOS, it's less stable than homebrew.
+  #
+  # Related Discussion: https://discourse.nixos.org/t/darwin-again/29331
+  environment.systemPackages = with pkgs; [
+    just
+    btop
+    cmake
+    fastfetch
+    fzf
+    gcc
+    git
+    go
+    lua
+    # luajit
+    neovim
+    ninja
+    nmap
+    parallel
+    pure-prompt
+    python3
+    ripgrep
+    stow
+    tmux
+    tree
+    vim
+    tree-sitter
+    wget
+    ncurses
+    zig
+    glow
+    zoxide
+  ];
+
 }
